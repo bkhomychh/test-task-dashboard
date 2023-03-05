1610132c37eed1cee5fc06216ecdc346409a199d
Bohdan Khomych <bod.khomych@gmail.com>
2023-03-06 01:03:03 +0200

    add dashboard markup

---
+++ docs/index.js
@@ -0,0 +1,38 @@
+const refs = {
+  openBtn: document.querySelector('.toggle--open'),
+  closeBtn: document.querySelector('.toggle--close'),
+  sidebar: document.querySelector('.sidebar'),
+};
+
+const mql = window.matchMedia('(max-width: 1100px)');
+
+window.addEventListener('click', onWindowCLick);
+mql.addEventListener('change', updateSidebarClass);
+
+function onWindowCLick({ target }) {
+  if (target.classList.contains('toggle')) {
+    refs.sidebar.classList.toggle('isHidden');
+    return;
+  }
+
+  if (!target.closest('.sidebar') && !refs.sidebar.classList.contains('isHidden')) {
+    hideSidebar();
+  }
+}
+
+function updateSidebarClass() {
+  if (window.innerWidth < 1100) {
+    hideSidebar();
+    return;
+  }
+
+  showSidebar();
+}
+
+function showSidebar() {
+  refs.sidebar.classList.remove('isHidden');
+}
+
+function hideSidebar() {
+  refs.sidebar.classList.add('isHidden');
+}
